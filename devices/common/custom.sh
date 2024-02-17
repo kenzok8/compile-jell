78886601501f265aa153d2d3bcef14c04c75cbea
kenzok8 <kenzok8@gmail.com>
2024-02-17 09:20:24 +0000

    fix patches

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 shopt -s extglob
-rm -rf feeds/jell/{diy,mt-drivers,shortcut-fe,luci-app-mtwifi,base-files}
+rm -rf feeds/jell/{diy,mt-drivers,shortcut-fe,luci-app-mtwifi,base-files,firewall,dnsmasq}
 
 for ipk in $(find feeds/jell/* -maxdepth 0 -type d);
 do
 	[[ "$(grep "KernelPackage" "$ipk/Makefile")" && ! "$(grep "BuildPackage" "$ipk/Makefile")" ]] && rm -rf $ipk || true
 done
-
-rm -rf feeds/jell/{luci-base,luci-mod-network,luci-mod-status,luci-mod-system}
 
 #<<'COMMENT'
 rm -Rf feeds/luci/{applications,collections,protocols,themes,libs,docs,contrib}
@@ -17,19 +15,23 @@ rm -Rf feeds/packages/!(lang|libs|devel|utils|net|multimedia)
 rm -Rf feeds/packages/multimedia/!(gstreamer1|ffmpeg)
 rm -Rf feeds/packages/libs/libcups
 rm -Rf feeds/packages/net/!(mosquitto|curl)
-rm -Rf feeds/base/package/{firmware}
+rm -Rf feeds/base/package/firmware
 rm -Rf feeds/base/package/network/!(services|utils)
 rm -Rf feeds/base/package/network/services/!(ppp)
 rm -Rf feeds/base/package/system/!(opkg|ubus|uci|ca-certificates)
 rm -Rf feeds/base/package/kernel/!(cryptodev-linux)
 #COMMENT
 
+status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/kenzok8/jell/actions/runs" | jq -r '.workflow_runs[0].status')
+while [[ "$status" == "in_progress" || "$status" == "queued" ]];do
+echo "wait 5s"
+sleep 5
+status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/kenzok8/jell/actions/runs" | jq -r '.workflow_runs[0].status')
+done
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a -p jell -f
 ./scripts/feeds install -a
-
-rm -rf feeds/packages/lang/golang
-svn export https://github.com/coolsnowwolf/packages/trunk/lang/golang feeds/packages/lang/golang
 
 sed -i 's/\(page\|e\)\?.acl_depends.*\?}//' `find package/feeds/jell/luci-*/luasrc/controller/* -name "*.lua"`
 # sed -i 's/\/cgi-bin\/\(luci\|cgi-\)/\/\1/g' `find package/feeds/jell/luci-*/ -name "*.lua" -or -name "*.htm*" -or -name "*.js"` &
@@ -57,11 +59,6 @@ sed -i "/mediaurlbase/d" package/feeds/*/luci-theme*/root/etc/uci-defaults/*
 
 sed -i '/WARNING: Makefile/d' scripts/package-metadata.pl
 
-if [ -f /usr/bin/python ]; then
-	ln -sf /usr/bin/python staging_dir/host/bin/python
-else
-	ln -sf /usr/bin/python3 staging_dir/host/bin/python
-fi
-ln -sf /usr/bin/python3 staging_dir/host/bin/python3
+
 cp -f devices/common/po2lmo staging_dir/host/bin/po2lmo
 chmod +x staging_dir/host/bin/po2lmo
