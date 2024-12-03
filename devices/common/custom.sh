3a0484d221d3f51ec5ea8afa6ec585d65a0b5dca
kenzok8 <kenzok8@gmail.com>
2024-12-03 23:48:26 +0800

    del.patch

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -5,8 +5,6 @@ rm -rf feeds/kiddin9/{diy,mt-drivers,shortcut-fe,luci-app-mtwifi,base-files,luci
 dnsmasq,firewall*,wifi-scripts,opkg,ppp,curl,luci-app-firewall,\
 nftables,fstools,wireless-regdb,libnftnl}
 
-curl -sfL https://raw.githubusercontent.com/openwrt/packages/master/lang/golang/golang/Makefile -o feeds/packages/lang/golang/golang/Makefile
-
 for ipk in $(find feeds/jell/* -maxdepth 0 -type d);
 do
 	[[ "$(grep "KernelPackage" "$ipk/Makefile")" && ! "$(grep "BuildPackage" "$ipk/Makefile")" ]] && rm -rf $ipk || true
